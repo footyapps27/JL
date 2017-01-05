f285f69a9d3831d9a55d69018a2a98edd3e22c3d
Samrat <footyapps27@gmail.com>
2017-01-05 19:47:46 +0800

    Added initial project

---
+++ JustLogin_MECS/JustLogin_MECS/ViewControllers/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  JustLogin_MECS
+//
+//  Created by Samrat on 5/1/17.
+//  Copyright © 2017 SMRT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    /***********************************/
+    // MARK - Properties
+    /***********************************/
+    @IBOutlet weak var btnSignUp: UIButton!
+    @IBOutlet weak var btnSignIn: UIButton!
+    
+    /***********************************/
+    // MARK - View Lifecycle
+    /***********************************/
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /***********************************/
+    // MARK - Actions
+    /***********************************/
+    
+
+    @IBAction func signUpTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+    }
+    
+}
+
