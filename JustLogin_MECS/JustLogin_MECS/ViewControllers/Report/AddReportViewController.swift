6d1ff792b78954c8e66cb1546c39ee9ea934892d
Samrat <samrat2702@gmail.com>
2017-03-09 11:08:45 +0800

    Add report remove viewDidLayoutSbview, & adjusted the topbar in storyboard.

--- JustLogin_MECS/JustLogin_MECS/ViewControllers/Report/AddReportViewController.swift
+++ JustLogin_MECS/JustLogin_MECS/ViewControllers/Report/AddReportViewController.swift
@@ -40,13 +40,6 @@ extension AddReportViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 44
     }
-    
-    override func viewDidLayoutSubviews() {
-        if let rect = self.navigationController?.navigationBar.frame {
-            let y = rect.size.height + rect.origin.y
-            self.tableView.contentInset = UIEdgeInsetsMake(y, 0, 0, 0)
-        }
-    }
 }
 /***********************************/
 // MARK: - Helpers
